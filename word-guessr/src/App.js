16dcb6150cc6b9a89727dca59b501a0fd89c9aa4
Michael McBride <michaelmcbride85@gmail.com>
2022-11-16 21:32:48 -0500

    adds landing page

--- word-guessr/src/App.js
+++ word-guessr/src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="title">
+          Word Guessr!
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start Game
         </a>
       </header>
     </div>
