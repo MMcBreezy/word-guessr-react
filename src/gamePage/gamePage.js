47d33afca76250b50c0916f57ba9d1822bb82dbf
Michael McBride <michaelmcbride85@gmail.com>
2023-02-04 15:09:31 -0500

    implemented guessed letter POST request

--- src/gamePage/gamePage.js
+++ src/gamePage/gamePage.js
@@ -1,26 +1,36 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
+import LogoSpin from "../logoSpin/logoSpin";
 import "./gamePage.css";
 import GuessrForm from "../guessrForm/guessrForm";
 
-
 function GamePage() {
+  const [gameState, setGameState] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // POST request using fetch inside useEffect React hook
     const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
     };
-    fetch('http://localhost:3001/game', requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data));
+    setLoading(true);
+    fetch("http://localhost:3001/game", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setGameState(data);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="title">
-          <GuessrForm maxLength="1" />
+          {loading ? (
+            <LogoSpin /> /*"loading..."*/
+          ) : (
+            <GuessrForm maxLength="1" data={gameState} />
+          )}
         </div>
       </header>
     </div>
