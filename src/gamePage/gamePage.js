c64ef699ab214faae96465989dcac306e5ae84ad
Michael McBride <michaelmcbride85@gmail.com>
2023-04-25 22:03:38 -0400

    cleanup from pairing session

--- src/gamePage/gamePage.js
+++ src/gamePage/gamePage.js
@@ -2,26 +2,20 @@ import { React, useEffect, useState } from "react";
 import LogoSpin from "../logoSpin/logoSpin";
 import "./gamePage.css";
 import GuessrForm from "../guessrForm/guessrForm";
+import { newGame } from "../helpers/apiHelper";
 
 function GamePage() {
   const [gameState, setGameState] = useState({});
   const [loading, setLoading] = useState(true);
   
-
-
   useEffect(() => {
-    // POST request using fetch inside useEffect React hook
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    };
     setLoading(true);
-    fetch(`${process.env.REACT_APP_API_URL}/game`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        setGameState(data);
-        setLoading(false);
-      });
+    const handleData = (data) => {
+      setGameState(data);
+      setLoading(false);
+    }
+
+    newGame(handleData);
   }, []);
 
   return (
@@ -31,7 +25,6 @@ function GamePage() {
       ) : (
         <GuessrForm maxLength="1" data={gameState} />
       )}
-
     </div>
   );
 }
---
+++ src/helpers/apiHelper.js
@@ -0,0 +1,25 @@
+module.exports = {
+    submitGuess: async function (id, guess, callback) {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ guess: guess }),
+        };
+        fetch(`${process.env.REACT_APP_API_URL}/game/${id}/guess`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            callback(data);
+        });
+    },
+    newGame: function (callback) {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        };
+        fetch(`${process.env.REACT_APP_API_URL}/game`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            callback(data);
+        });
+    },
+}
